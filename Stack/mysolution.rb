b717a52097af1ec10a0d722a222bf72f310749d3
emiliocm9 <emilio.contreras97@gmail.com>
2020-10-20 11:50:57 -0500

    docs: stack mysolution update

--- Stack/mysolution.rb
+++ Stack/mysolution.rb
@@ -9,45 +9,41 @@ class Node
 end
 
 class Stack
-  attr_accessor :top, :minimum
   def initialize
-    @top = nil
-    @min = nil
+    @first = nil
   end
-  
-  def push(number)
-    @top = Node.new(number, @top)
-    @min = @top if @min.nil?
-    @min = Node.new(number, @min) if number < minimum
+
+  def push(value)
+    @first = Node.new(value, @first)
   end
-  
+  alias_method :"<<", :push
+
   def pop
-    # your code here
-    return if @top.nil?
-    @min = @min.next_node if @top.value.eql? @min.value
-    value = @top.value
-    @top = @top.next_node
+    raise "Stack is empty" if is_empty?
+    value = @first.value
+    @first = @first.next_node
     value
   end
-  
-  def minimum
-    @min.value unless @min.nil?
+
+  def is_empty?
+    @first.nil?
   end
 end
 
 stack = Stack.new
 stack.push(3)
-stack.push(5)
+stack << 2
 puts stack.pop
-# => 5
-
-stack.push(2)
-stack.push(7)
 puts stack.pop
-# => 7
-
-puts stack.pop
-# => 2
-
-puts stack.pop
-# => 3
+## => 5
+#
+#stack.push(2)
+#stack.push(7)
+#puts stack.pop
+## => 7
+#
+#puts stack.pop
+## => 2
+#
+#puts stack.pop
+## => 3
